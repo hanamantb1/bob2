b9497fda4be94e373a79bffd4d16c8d316e87537
Hanamant Batakurki <hanamant.batakurki@speridian.com>
2023-03-16 17:48:13 +0530

    column name chnaged

--- src/main/java/com/medicare/bob/entity/Customer.java
+++ src/main/java/com/medicare/bob/entity/Customer.java
@@ -1,6 +1,5 @@
 package com.medicare.bob.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,223 +19,341 @@ public class Customer {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long CustomerID;
+	private Long customer_id;
 	
-	private String TenantID;
-	private String AgentID;
-	private String First_Name;
-	private String Middle_Name;
-	private String Last_Name;
-	private String Suffix;
-	private String SSN;
-	private String DOB;
-	private String Gender;
-	
-	private String Address_Line1;
-	private String Address_Line2;
-	
-	private String City;
-	private String State;
-	private String Zip_Ext;
-	private String Zipcode;
-	private String County;
-	private String Phone;
-	private String Cell_Phone;
-	private String Email;
-	private String Medicare;
-	private String PartA_Eff;
-	private String PartB_Eff;
-	private String Plan_Name;
-	private String Spouse_Firstname;
-	private String Spouse_Lastname;
-	private String Spouse_Suffix;
-	private String Spouse_DOB;
-	private String Created_Date;
-	
-//	@Column(name="LastUpdated_Date")
-//	private String LastUpdated_Date; 
-	
-	public Long getCustomerID() {
-		return CustomerID;
+	public Long getCustomer_id() {
+		return customer_id;
 	}
-	public void setCustomerID(Long customerID) {
-		CustomerID = customerID;
-	}
-	public String getTenantID() {
-		return TenantID;
-	}
-	public void setTenantID(String tenantID) {
-		TenantID = tenantID;
-	}
-	public String getAgentID() {
-		return AgentID;
-	}
-	public void setAgentID(String agentID) {
-		AgentID = agentID;
-	}
-	public String getFirst_Name() {
-		return First_Name;
-	}
-	public void setFirst_Name(String first_Name) {
-		First_Name = first_Name;
-	}
-	public String getMiddle_Name() {
-		return Middle_Name;
-	}
-	public void setMiddle_Name(String middle_Name) {
-		Middle_Name = middle_Name;
-	}
-	public String getLast_Name() {
-		return Last_Name;
-	}
-	public void setLast_Name(String last_Name) {
-		Last_Name = last_Name;
-	}
-	public String getSuffix() {
-		return Suffix;
-	}
-	public void setSuffix(String suffix) {
-		Suffix = suffix;
-	}
-	public String getSSN() {
-		return SSN;
-	}
-	public void setSSN(String sSN) {
-		SSN = sSN;
-	}
-	public String getDOB() {
-		return DOB;
-	}
-	public void setDOB(String dOB) {
-		DOB = dOB;
-	}
-	public String getGender() {
-		return Gender;
-	}
-	public void setGender(String gender) {
-		Gender = gender;
-	}
-	public String getAddress_Line1() {
-		return Address_Line1;
-	}
-	public void setAddress_Line1(String address_Line1) {
-		Address_Line1 = address_Line1; 
-	}
-	public String getAddress_Line2() {
-		return Address_Line2;
-	}
-	public void setAddress_Line2(String address_Line2) {
-		Address_Line2 = address_Line2;
-	}
-	public String getCity() {
-		return City;
-	}
-	public void setCity(String city) {
-		City = city;
-	}
-	public String getState() {
-		return State;
-	}
-	public void setState(String state) {
-		State = state;
-	}
-	public String getZip_Ext() {
-		return Zip_Ext;
-	}
-	public void setZip_Ext(String zip_Ext) {
-		Zip_Ext = zip_Ext;
-	}
-	public String getZipcode() {
-		return Zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		Zipcode = zipcode;
-	}
-	public String getCounty() {
-		return County;
-	}
-	public void setCounty(String county) {
-		County = county;
-	}
-	public String getPhone() {
-		return Phone;
-	}
-	public void setPhone(String phone) {
-		Phone = phone;
-	}
-	public String getCell_Phone() {
-		return Cell_Phone;
-	}
-	public void setCell_Phone(String cell_Phone) {
-		Cell_Phone = cell_Phone;
-	}
-	public String getEmail() {
-		return Email;
-	}
-	public void setEmail(String email) {
-		Email = email;
-	}
-	public String getMedicare() {
-		return Medicare;
-	}
-	public void setMedicare(String medicare) {
-		Medicare = medicare;
-	}
-	public String getPartA_Eff() {
-		return PartA_Eff;
-	}
-	public void setPartA_Eff(String partA_Eff) {
-		PartA_Eff = partA_Eff;
-	}
-	public String getPartB_Eff() {
-		return PartB_Eff;
-	}
-	public void setPartB_Eff(String partB_Eff) {
-		PartB_Eff = partB_Eff;
-	}
-	public String getPlan_Name() {
-		return Plan_Name;
-	}
-	public void setPlan_Name(String plan_Name) {
-		Plan_Name = plan_Name;
-	}
-	public String getSpouse_Firstname() {
-		return Spouse_Firstname;
-	}
-	public void setSpouse_Firstname(String spouse_Firstname) {
-		Spouse_Firstname = spouse_Firstname;
-	}
-	public String getSpouse_Lastname() {
-		return Spouse_Lastname;
-	}
-	public void setSpouse_Lastname(String spouse_Lastname) {
-		Spouse_Lastname = spouse_Lastname;
-	}
-	public String getSpouse_Suffix() {
-		return Spouse_Suffix;
-	}
-	public void setSpouse_Suffix(String spouse_Suffix) {
-		Spouse_Suffix = spouse_Suffix;
-	}
-	public String getSpouse_DOB() {
-		return Spouse_DOB;
-	}
-	public void setSpouse_DOB(String spouse_DOB) {
-		Spouse_DOB = spouse_DOB;
-	}
-	public String getCreated_Date() {
-		return Created_Date;
-	}
-	public void setCreated_Date(String created_Date) {
-		Created_Date = created_Date;
-	}
-//	public String getLastUpdated_Date() {
-//		return LastUpdated_Date;
-//	}
-//	public void setLastUpdated_Date(String lastUpdated_Date) {
-//		LastUpdated_Date = lastUpdated_Date;
-//	}
-//	
 
+
+	public void setCustomer_id(Long customer_id) {
+		this.customer_id = customer_id;
+	}
+
+
+	public String getTenant_id() {
+		return tenant_id;
+	}
+
+
+	public void setTenant_id(String tenant_id) {
+		this.tenant_id = tenant_id;
+	}
+
+
+	public String getAgent_id() {
+		return agent_id;
+	}
+
+
+	public void setAgent_id(String agent_id) {
+		this.agent_id = agent_id;
+	}
+
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+
+	public String getMiddle_name() {
+		return middle_name;
+	}
+
+
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+
+	public String getSsn() {
+		return ssn;
+	}
+
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+
+	public String getDob() {
+		return dob;
+	}
+
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public String getAddress_line1() {
+		return address_line1;
+	}
+
+
+	public void setAddress_line1(String address_line1) {
+		this.address_line1 = address_line1;
+	}
+
+
+	public String getAddress_line2() {
+		return address_line2;
+	}
+
+
+	public void setAddress_line2(String address_line2) {
+		this.address_line2 = address_line2;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getZip_Ext() {
+		return zip_Ext;
+	}
+
+
+	public void setZip_Ext(String zip_Ext) {
+		this.zip_Ext = zip_Ext;
+	}
+
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+
+	public String getCounty() {
+		return county;
+	}
+
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getCell_Phone() {
+		return cell_Phone;
+	}
+
+
+	public void setCell_Phone(String cell_Phone) {
+		this.cell_Phone = cell_Phone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getMedicare() {
+		return medicare;
+	}
+
+
+	public void setMedicare(String medicare) {
+		this.medicare = medicare;
+	}
+
+
+	public String getParta_eff() {
+		return parta_eff;
+	}
+
+
+	public void setParta_eff(String parta_eff) {
+		this.parta_eff = parta_eff;
+	}
+
+
+	public String getPartb_eff() {
+		return partb_eff;
+	}
+
+
+	public void setPartb_eff(String partb_eff) {
+		this.partb_eff = partb_eff;
+	}
+
+
+	public String getPlan_name() {
+		return plan_name;
+	}
+
+
+	public void setPlan_name(String plan_name) {
+		this.plan_name = plan_name;
+	}
+
+
+	public String getSpouse_firstname() {
+		return spouse_firstname;
+	}
+
+
+	public void setSpouse_firstname(String spouse_firstname) {
+		this.spouse_firstname = spouse_firstname;
+	}
+
+
+	public String getSpouse_lastname() {
+		return spouse_lastname;
+	}
+
+
+	public void setSpouse_lastname(String spouse_lastname) {
+		this.spouse_lastname = spouse_lastname;
+	}
+
+
+	public String getSpouse_suffix() {
+		return spouse_suffix;
+	}
+
+
+	public void setSpouse_suffix(String spouse_suffix) {
+		this.spouse_suffix = spouse_suffix;
+	}
+
+
+	public String getSpouse_dob() {
+		return spouse_dob;
+	}
+
+
+	public void setSpouse_dob(String spouse_dob) {
+		this.spouse_dob = spouse_dob;
+	}
+
+
+	public String getCreated_date() {
+		return created_date;
+	}
+
+
+	public void setCreated_date(String created_date) {
+		this.created_date = created_date;
+	}
+
+
+	public String getLastupdated_date() {
+		return lastupdated_date;
+	}
+
+
+	public void setLastupdated_date(String lastupdated_date) {
+		this.lastupdated_date = lastupdated_date;
+	}
+
+
+	private String tenant_id;
+	private String agent_id;
+	private String first_name;
+	private String middle_name;
+	private String last_name;
+	private String suffix;
+	private String ssn;
+	private String dob;
+	private String gender;
+	
+	private String address_line1;
+	private String address_line2;
+	
+	private String city;
+	private String state;
+	private String zip_Ext;
+	private String zipcode;
+	private String county;
+	private String phone;
+	private String cell_Phone;
+	private String email;
+	private String medicare;
+	private String parta_eff;
+	private String partb_eff;
+	private String plan_name;
+	private String spouse_firstname;
+	private String spouse_lastname;
+	private String spouse_suffix;
+	private String spouse_dob;
+	private String created_date;
+	
+
+	private String lastupdated_date; 
+	
 	
 }
