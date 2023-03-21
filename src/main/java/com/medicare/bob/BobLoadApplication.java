10b48ace380f88f89be54839ede538b56e64afdb
Hanamant Batakurki <hanamant.batakurki@speridian.com>
2023-03-21 12:04:04 +0530

    intial project set up

--- src/main/java/com/medicare/bob/BobLoadApplication.java
+++ src/main/java/com/medicare/bob/BobLoadApplication.java
@@ -3,7 +3,9 @@ package com.medicare.bob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableSwagger2
 public class BobLoadApplication {
 
 	public static void main(String[] args) {
